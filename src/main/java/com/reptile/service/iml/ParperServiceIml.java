8549484768a2ae85d6f3ebc20e4e39b7d5856ab0
lkxiaojian <18513075404@163.com>
2019-02-20 11:04:01 +0800

    算法对接，虚拟id

--- src/main/java/com/reptile/service/iml/ParperServiceIml.java
+++ src/main/java/com/reptile/service/iml/ParperServiceIml.java
@@ -11,6 +11,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 @Service
 public class ParperServiceIml implements ParperService {
 
@@ -19,17 +20,23 @@ public class ParperServiceIml implements ParperService {
 
     @Resource
     private Article1Mapper article1Mapper;
+
     @Override
     public Map<String, Object> getData(int rows, int page, String type) throws Exception {
-        Map<String,Object> map=new HashMap<>();
-        map.put("rows",rows);
-        map.put("page",(page-1)*rows);
-        map.put("type",type);
+        if ("1".equals(type)) {
+            type = "机器人";
+        } else if ("2".equals(type)) {
+            type = "机械工程学报";
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("rows", rows);
+        map.put("page", (page - 1) * rows);
+        map.put("type", type);
 
         List<Map<String, Object>> maps = academicPaperMapper.getData(map);
-        int count=  academicPaperMapper.getCountByName(map);
+        int count = academicPaperMapper.getCountByName(map);
 
-        Map<String,Object> resultMap=new HashMap<>();
+        Map<String, Object> resultMap = new HashMap<>();
         //把pdf 转成字符串
 //        for(int i=0;i<maps.size();i++){
 //            Object pdf_path = maps.get(i).get("pdf_path");
@@ -37,19 +44,20 @@ public class ParperServiceIml implements ParperService {
 //                maps.get(i).put("txt",PDFtoContent.getContent("D:/File/"+pdf_path.toString()));
 //            }
 //        }
-        int num=count - rows*page;
-        if(num<0){
-            num=0;
+        int num = count - rows * page;
+        if (num < 0) {
+            num = 0;
         }
-        resultMap.put("code",0);
-        resultMap.put("message","请求成功");
+        resultMap.put("code", 0);
+        resultMap.put("message", "请求成功");
         resultMap.put("count", num);
-        resultMap.put("result",maps);
+        resultMap.put("result", maps);
         return resultMap;
     }
 
     /**
      * 获取微信文章
+     *
      * @param rows
      * @param page
      * @param type
@@ -58,13 +66,13 @@ public class ParperServiceIml implements ParperService {
      */
     @Override
     public Map getWxData(int rows, int page, String type) throws Exception {
-        Map<String,Object> paremMap=new HashMap<>();
-        paremMap.put("rows",rows);
-        paremMap.put("page",(page - 1) * rows);
-        paremMap.put("type",type);
+        Map<String, Object> paremMap = new HashMap<>();
+        paremMap.put("rows", rows);
+        paremMap.put("page", (page - 1) * rows);
+        paremMap.put("type", type);
 
         int wxDataCount = article1Mapper.getWxDataCount(paremMap);
-        List<Map<String, Object>> maps=  article1Mapper.getWxData(paremMap);
+        List<Map<String, Object>> maps = article1Mapper.getWxData(paremMap);
         for (int i = 0; i < maps.size(); i++) {
             Object details_div = maps.get(i).get("details_txt");
             Object article_title = maps.get(i).get("article_title");
@@ -72,14 +80,14 @@ public class ParperServiceIml implements ParperService {
             byte[] details_divbytes = (byte[]) details_div;
             if (details_div != null && article_title != null) {
                 try {
-                    String s = new String(details_divbytes, "UTF-8").replaceAll(" ","").replaceAll("\\s","").replaceAll(",","，").replaceAll("!","！").replaceAll("\\.","。").replaceAll("\\[","】")
-                            .replaceAll("]","】").replaceAll("\\(","（").replaceAll("\\)","）").replaceAll("\\|","|")
-                            .replaceAll("-","—");
-                    String s1 = article_title.toString().replaceAll(",", "，").replaceAll("!","！").replaceAll("\\.","。").replaceAll("\\[","】")
-                            .replaceAll("]","】").replaceAll("\\(","（").replaceAll("\\)","）").replaceAll("\\|","|")
-                            .replaceAll("-","—").replaceAll(" ","").replaceAll("\\s","");
-                    s =s.replaceAll(s1, "");
-                    maps.get(i).put("details_txt",s);
+                    String s = new String(details_divbytes, "UTF-8").replaceAll(" ", "").replaceAll("\\s", "").replaceAll(",", "，").replaceAll("!", "！").replaceAll("\\.", "。").replaceAll("\\[", "】")
+                            .replaceAll("]", "】").replaceAll("\\(", "（").replaceAll("\\)", "）").replaceAll("\\|", "|")
+                            .replaceAll("-", "—");
+                    String s1 = article_title.toString().replaceAll(",", "，").replaceAll("!", "！").replaceAll("\\.", "。").replaceAll("\\[", "】")
+                            .replaceAll("]", "】").replaceAll("\\(", "（").replaceAll("\\)", "）").replaceAll("\\|", "|")
+                            .replaceAll("-", "—").replaceAll(" ", "").replaceAll("\\s", "");
+                    s = s.replaceAll(s1, "");
+                    maps.get(i).put("details_txt", s);
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
@@ -87,7 +95,7 @@ public class ParperServiceIml implements ParperService {
         }
         Map<String, Object> map = new HashMap<>();
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("count", wxDataCount- page * rows);
+        resultMap.put("count", wxDataCount - page * rows);
         resultMap.put("txt", maps);
         map.put("code", 0);
         map.put("result", resultMap);
